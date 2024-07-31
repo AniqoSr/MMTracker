5ad677ce9ed0e30b6387627a6bc49f361bbb247b
AnikoSr <anikosyahraramadhani@outlook.com>
2024-08-01 01:22:29 +0700

    Fix Bugs

---
+++ src/main/java/net/justforfun/MMTracker/configs/ConfigManager.java
@@ -0,0 +1,47 @@
+package net.justforfun.MMTracker.configs;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigManager {
+    private final JavaPlugin plugin;
+    private FileConfiguration config;
+    private File configFile;
+
+    public ConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+        setupConfig();
+    }
+
+    private void setupConfig() {
+        configFile = new File(plugin.getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            plugin.saveResource("config.yml", false);
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public void reloadConfig() {
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JavaPlugin getPlugin() {
+        return plugin;
+    }
+}
